4ff765af250625e972161fa6e6e44aba08dac5a3
killthebee <superhotfire447@yandex.ru>
2021-04-22 11:40:00 +0300

    add phrases to repo

---
+++ year_phrases.py
@@ -0,0 +1,10 @@
+PHRASES = {
+    1957: "First Sputnik",
+    1961: "Gagarin flew!",
+    1969: "Armstrong got on the moon!",
+    1971: "First orbital space station Salute-1",
+    1981: "Flight of the Shuttle Columbia",
+    1998: 'ISS start building',
+    2011: 'Messenger launch to Mercury',
+    2020: "Take the plasma gun! Shoot the garbage!",
+}
